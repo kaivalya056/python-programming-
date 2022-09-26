24bf477b1fa091cc044f80ab7a9d5882b126fe6d
kaivalya056 <112381426+kaivalya056@users.noreply.github.com>
2022-09-26 11:08:07 +0530

    Add files via upload

---
+++ convert a given number .py output.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/kolla/AppData/Local/Programs/Python/Python310/convert a given number .py
+years =  0
+weeks =  28 
+days =  4
